271ce313baa27e011de0da1ebd4ed53f0b7c07a0
Daryl's Laptop <35038741+dwrue1@users.noreply.github.com>
2018-02-06 18:47:22 -0500

    Rebuilt Jar Files

--- src/DigipuffLearnsJava/HaikuDisplay.java
+++ src/DigipuffLearnsJava/HaikuDisplay.java
@@ -4,7 +4,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
